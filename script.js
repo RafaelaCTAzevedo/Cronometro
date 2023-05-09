0a2f3a5adeaf15290f84ca31fdf542ec69e3cb14
Rafaela Azevedo <rafaela.c.t.azevedo@gmail.com>
2023-05-08 21:45:58 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,84 @@
+//Botão de se inscrever
+const subscribeButton = document.querySelector('#subscribe')
+const closeModalButton = document.querySelector('.close-modal')
+const submitButton = document.querySelector('.submit-button')
+const modal = document.querySelector('#modal')
+const form = document.querySelector('#form')
+
+const successMessageBox = document.querySelector('#success-submit')
+const errorMessageBox = document.querySelector('#error-submit')
+
+
+subscribeButton.addEventListener('click', () => {
+    modal.classList.toggle('active')
+    form.classList.toggle('active-drop')
+  })
+  
+  closeModalButton.addEventListener('click', (e) => {
+    e.preventDefault()
+  
+    modal.classList.toggle('active')
+    form.classList.toggle('active-drop')
+    form.reset()
+  })
+  
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (
+      errorMessageBox.classList.contains('error') ||
+      successMessageBox.classList.contains('success')
+    )
+      return
+    if (!form.reportValidity()) {
+      errorMessageBox.classList.toggle('error')
+      setTimeout(() => {
+        errorMessageBox.classList.toggle('error')
+      }, 2000)
+    } else {
+      successMessageBox.classList.toggle('success')
+      setTimeout(() => {
+        successMessageBox.classList.toggle('success')
+      }, 2000)
+      form.reset()
+    }
+  })
+
+ //Cronometro 
+  const daysElement = document.querySelector('#days')
+  const hoursElement = document.querySelector('#hours')
+  const minutesElement = document.querySelector('#minutes')
+  const secondsElement = document.querySelector('#seconds')
+  
+  window.onload = () => {
+    setInterval(startTimer, 1000)
+  }
+  
+  let staticDate = new Date('june 09, 2023 23:55:45').getTime()
+  
+  const updateCountdown = (d, h, m, s) => {
+    daysElement.innerText = d
+    hoursElement.innerText = h
+    minutesElement.innerText = m
+    secondsElement.innerText = s
+  }
+  
+  const startTimer = () => {
+    let currentDate = new Date().getTime()
+    let remainingDate = staticDate - currentDate
+  
+    let days = String(Math.floor(remainingDate / (1000 * 60 * 60 * 24))).padStart(
+      2,
+      '0'
+    )
+    let hours = String(
+      Math.floor((remainingDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    ).padStart(2, '0')
+    let minutes = String(
+      Math.floor((remainingDate % (1000 * 60 * 60)) / (1000 * 60))
+    ).padStart(2, '0')
+    let seconds = String(
+      Math.floor((remainingDate % (1000 * 60)) / 1000)
+    ).padStart(2, '0')
+  
+    updateCountdown(days, hours, minutes, seconds)
+  }
